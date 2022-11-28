bc0ef5b9f187a80365d1d341b636232bc24a03a8
M_Sova <MyMail@mail.ru>
2022-11-28 23:19:10 +0300

    Task_25

---
+++ Task25_AinDegreeB/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int Exponent (int num1, int num2)
+{
+    int result = 1;
+    for (int i=1; i<=num2; i++)
+    {
+        result = num1*result;
+    }
+    return result;
+}
+if (b>0)
+{
+    int expo = Exponent (a, b);
+    Console.WriteLine($"Число {a} в степени {b} ---> {expo}");
+}
+else
+{
+    Console.WriteLine("Введите вещественное число");
+}
