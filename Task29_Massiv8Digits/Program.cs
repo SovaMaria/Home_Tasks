7f15ff91adb9082f129e07eaf959e3e4be390d69
M_Sova <MyMail@mail.ru>
2022-11-29 00:38:57 +0300

    Task29_Massiv8Digits

---
+++ Task29_Massiv8Digits/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int [] array = new int [8];
+void RandomArray (int [] array)
+{
+    Random rnd = new Random ();
+    for (int i=0; i<array.Length; i++)
+    {
+        array [i] = rnd.Next (0, 100);
+    }
+}
+void PrintArray (int [] arr)
+{
+    Console.Write ("[");
+    for (int i=0; i<arr.Length; i++)
+    {
+        if (i<arr.Length-1)
+             Console.Write ($"{arr[i]}, ");
+             
+        else
+             Console.Write ($"{arr[i]}");
+    }
+Console.Write ("]");
+}
+
+RandomArray (array);
+PrintArray (array);
