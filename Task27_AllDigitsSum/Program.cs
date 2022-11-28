1903e7942b0a993b3bf69bec894d87282c4abc81
M_Sova <MyMail@mail.ru>
2022-11-28 23:54:31 +0300

    Task27_AllDigitsSum

---
+++ Task27_AllDigitsSum/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int AllDigitsSum (int num)
+{
+    int summa = 0;
+    while (num > 0)
+    {
+        summa = summa + num % 10;
+        num = num / 10;
+    }
+    return summa;
+}
+if (number<0) number = -number;
+int SummaDigit = AllDigitsSum(number);
+Console.WriteLine($"Сумма цифр числа {number} ---> {SummaDigit}");
