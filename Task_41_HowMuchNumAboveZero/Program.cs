65e64d362eb4a8a13d45dea0402de9d7ff129cd1
M_Sova <MyMail@mail.ru>
2022-12-05 16:23:01 +0300

    Task_41_HowMuchNumAboveZero

---
+++ Task_41_HowMuchNumAboveZero/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.Write("Введите несколько целых чисел через пробел: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int NumAboveZero(int[] arr)
+{
+    int numbers = 0;
+ 
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] > 0) numbers++;
+        }
+        return numbers;
+}
+
+int numbers = NumAboveZero(arr);
+Console.WriteLine($"Количество чисел больше нуля: {numbers}");
+
+
+
